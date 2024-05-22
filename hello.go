46d90380f6d8edac5719b12eeee107a8adfc13b1
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2024-05-21 21:15:32 -0300

    Documented the implementations

--- hello.go
+++ hello.go
@@ -12,16 +12,22 @@ import (
 )
 
 func main() {
-
+	/// Getting the urls and storing on a variable
 	urls := openFile()
 
+	/// Creating an iteration through all urls and creating a request with each one
 	for i := range urls {
 		request(urls[i])
 	}
+
+	/// Shows the logs
 	printLogs()
+
+	/// Clears the logs
 	clearLogs()
 }
 
+// / Creates a request on a given url and treats the return
 func request(site string) {
 	result, err := http.Get(site)
 
@@ -41,6 +47,7 @@ func request(site string) {
 	}
 }
 
+// / Opens the urls.txt file where all urls are stored and returns the urls to be used
 func openFile() []string {
 	var result []string
 
@@ -72,6 +79,7 @@ func openFile() []string {
 	return result
 }
 
+// / Creates a log file to store the request results
 func createLog(text string, status bool) {
 	file, err := os.OpenFile("loggin.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
 
@@ -93,6 +101,7 @@ func createLog(text string, status bool) {
 
 }
 
+// / Shows logs on the terminal
 func printLogs() {
 	file, err := os.ReadFile("loggin.txt")
 	if err != nil {
@@ -103,6 +112,7 @@ func printLogs() {
 	fmt.Println(string(file))
 }
 
+// / Deletes the logs file
 func clearLogs() {
 	err := os.Remove("loggin.txt")
 	if err != nil {
